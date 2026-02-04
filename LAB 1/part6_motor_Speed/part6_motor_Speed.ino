4db62fa16853b8d904f37b2d81ecd535835fdc91
Patrick <patrickflipse@gmail.com>
2026-02-04 12:09:21 -0500

    Lab

---
+++ LAB 1/part6_motor_Speed/part6_motor_Speed.ino
@@ -0,0 +1,46 @@
+#define POT 4 // potentiometer
+#define PWM 5
+#define AI1 6
+#define AI2 7
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(AI1, OUTPUT);
+  pinMode(AI2, OUTPUT);
+  pinMode(PWM, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  uint16_t potValue = analogRead(POT);
+  int newPot = map(potValue, 12 ,3300, 0, 255);
+  int newPotValue = abs(newPot - 122);
+  newPotValue = map(newPotValue,0, 122, 0, 255);
+  newPotValue = constrain(newPotValue, 0, 255);
+  analogWrite(PWM, newPotValue); 
+  Serial.println(newPotValue);
+
+  if(newPot > 122)
+  {
+    digitalWrite(AI1, HIGH);
+    digitalWrite(AI2, LOW);
+  }
+  else if(newPot < 122)
+  {
+    digitalWrite(AI1, LOW);
+    digitalWrite(AI2, HIGH);
+  }
+  else
+  {
+    digitalWrite(AI1, LOW);
+    digitalWrite(AI2, LOW);
+  }
+
+
+  
+  delay(50);
+}
