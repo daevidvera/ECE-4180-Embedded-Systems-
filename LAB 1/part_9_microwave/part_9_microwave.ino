7ece4b9363087c1fa428cb882dc9468c48a0c657
Patrick <patrickflipse@gmail.com>
2026-02-05 10:01:41 -0500

    Lab 9 edits

--- LAB 1/part_9_microwave/part_9_microwave.ino
+++ LAB 1/part_9_microwave/part_9_microwave.ino
@@ -2,33 +2,31 @@
 #define PWM 5
 #define AI1 6
 #define AI2 7
+//define speaker pin?
 
+enum state_nodes {START, COOKING, PAUSED, DONE};
+enum state_nodes current_state = START;
+int cookTime = 0;  // Global variable
 
-
-enum state_nodes = {START, COOKING, PAUSED, DONE};
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("Enter a number: ")
   pinMode(AI1, OUTPUT);
   pinMode(AI2, OUTPUT);
   pinMode(PWM, OUTPUT);
+  //speaker pin??
 
-  enum state_codes current_state = START;
 }
 
 void loop() {
 
   // put your main code here, to run repeatedly:
-  uint16_t potValue = analogRead(POT);
-  int newPot = map(potValue, 12 ,3300, 0, 255);
-  int newPotValue = abs(newPot - 122);
-  newPotValue = map(newPotValue,0, 122, 0, 255);
-  newPotValue = constrain(newPotValue, 0, 255);
-  analogWrite(PWM, newPotValue); 
-  Serial.println(newPotValue);
+  // uint16_t potValue = analogRead(POT); //POT INPUT
+  // analogWrite(PWM, 50);  // PWM OUTPUT
+  // Serial.println(newPotValue);
 
+  /// POT AND MOTOR DRIVER LOGIC
   // if(newPot > 122)
   // {
   //   digitalWrite(AI1, HIGH);
@@ -48,37 +46,53 @@ void loop() {
   switch (current_state) {
 
     case START:
-     int time = Serial.parseInt();
-     current_state = COOKING;
+     // LED turns blue
+     Serial.println("Enter a number: ");
+     while (Serial.available() == 0){
+      delay(10);
+     }
+     cookTime = Serial.parseInt();
+     while(Serial.available() > 0) {
+        Serial.read();
+      }
+     if (cookTime > 0){
+      current_state = COOKING;
+     }
      break;
 
     case COOKING:
-    while(time >= 0){
+      //yellow LED
+      //add conditional of pot turning or door opening if so then current state becomes PAUSED
+      while(cookTime > 0){
 
-      if(POT <= 0) {
-        current_state = PAUSED;
-        break;
+        if(POT <= 0) {
+          current_state = PAUSED;
+          break;
+        }
+        
+        Serial.println(cookTime);
+        cookTime = cookTime -1;
+        delay(1000);
+
       }
-      
-      Serial.println(time);
-      time = time -1;
-      delay(100);
-
-    }
-    current_state = DONE;
-      
-
-    break;
+      current_state = DONE;
+      break;
 
     case PAUSED:
+    // led turns red
+    // waits until pot to close again and then goes back to COOKING state
     break;
 
     case DONE:
+      Serial.println("DONE!");
+      //LED turns green
+      // speaker beeps
+      //make condtional on waiting for door to open or pot
+      // then LED turns red
+      // then waits for door to close or pot turn again to go to start state
+      current_state = START;
     break;
-    
-    
   }
-
 
   
   delay(50);
